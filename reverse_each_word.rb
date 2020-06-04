523c27e5218a94cab0a18ace68ea9175c463dfb1
Michael Schneider <mic.sch91@gmail.com>
2020-06-04 19:34:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  words = sentence.split (" ")
+  words = sentence.split(" ")
   backwards = []
   backwards = words.collect { |word| word.reverse }
   backwards.join(" ")
